e2e13ccd0aaf8141900344bfcfdfa00e4fe51a94
Miguel Angel Romero <miguelrop@gmail.com>
2019-04-16 19:57:31 +0200

    Modificamos el SW

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // imports
-importScripts('/cursopwaadvance/06-twittor/js/sw-utils.js');
-
+//importScripts('/cursopwaadvance/06-twittor/js/sw-utils.js');
+importScripts('js/sw-utils.js');
 const STATIC_CACHE = 'static-v1';
 const DYNAMIC_CACHE = 'dynamic-v1';
 const INMUTABLE_CACHE = 'inmutable-v1';
